b0622f6a2e9693a9b0dc6706d247cdfd7f2d43f0
grindstafftd <grindstafftd@MLC22001>
2022-09-01 17:24:49 -0400

    Add project files.

---
+++ Lab2Timothy/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Question One:");
+        Console.WriteLine("Please Enter a Number");
+        int NumberOne = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("PLease Enter a Second Number");
+        int NumberTwo = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine($"{NumberOne} + {NumberTwo} = {NumberOne + NumberTwo}");
+
+        Console.WriteLine("Question 2:");
+        Console.WriteLine("Input a Number to Multiply");
+        int Multiple = Convert.ToInt32(Console.ReadLine());
+        int[] Multi = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+        foreach (int i in Multi)
+        {
+
+            Console.WriteLine($"{Multiple} x {Multi} = {Multiple * Multi}");
+        }
+
+
+    }
+}
